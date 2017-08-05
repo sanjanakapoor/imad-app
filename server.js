806f5f879fa3921c558fea989b42eca5b0a87992
sanjanakapoor <30627656+sanjanakapoor@users.noreply.github.com>
2017-08-05 04:22:06 -0930

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.get('/article-two',function (req, res){
 
 
 app.get('/article-three',function (req, res){
-    res.send('artical three requested and will be served here');
+     res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
 });
 
 
